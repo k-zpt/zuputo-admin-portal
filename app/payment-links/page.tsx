0bf900444d760768333eea66543de3f309195410
Kwame <kwame@zuputo.com>
2026-01-21 06:48:38 +0000

    resolve build issues

--- app/payment-links/page.tsx
+++ app/payment-links/page.tsx
@@ -940,6 +940,7 @@ export default function PaymentLinksPage() {
         setInvoiceData({
           countryId: '',
           currencyId: '',
+          currencyCode: '',
           tax: '',
           discountPercentage: '',
           amountPaid: '',
